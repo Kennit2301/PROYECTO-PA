8fc1e6b95b918a26157eb90304398b8c081b3fe9
Kennit Saucedo <l22280944@toluca.tecnm.mx>
2024-10-06 19:51:23 -0600

    Stats.java

--- Stats.java
+++ Stats.java
@@ -1,5 +1,6 @@
 package entities;
 
+// Definición del enumerado Stats, que representa las diferentes estadísticas que un jugador o enemigo puede tener.
 public enum Stats {
     HP, MAX_HP, MP, MAX_MP, ATTACK, DEFENSE, SPEED, DEXTERITY, LUCK, ACCURACY, EVASION, CRITICAL_HIT_CHANCE, CRITICAL_HIT_DAMAGE
 }
