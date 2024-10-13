958b4db6db78c5b05f8bb74d73dce31c4e4b18c4
Kennit Saucedo <l22280944@toluca.tecnm.mx>
2024-10-12 22:05:04 -0600

    Create armas.java

---
+++ armas.java
@@ -0,0 +1,36 @@
+package items;
+
+// Ejemplo de arma: Espada de hierro
+public class IronSword extends Weapon {
+    public IronSword() {
+        super("Iron Sword", "A basic iron sword.", 10);
+    }
+}
+
+// Ejemplo de arma: Arco largo
+public class LongBow extends Weapon {
+    public LongBow() {
+        super("Long Bow", "A long-range bow.", 8);
+    }
+}
+
+// Ejemplo de arma: Hacha de batalla
+public class BattleAxe extends Weapon {
+    public BattleAxe() {
+        super("Battle Axe", "A heavy axe for brutal attacks.", 15);
+    }
+}
+
+// Ejemplo de arma: Daga venenosa
+public class PoisonDagger extends Weapon {
+    public PoisonDagger() {
+        super("Poison Dagger", "A dagger that inflicts poison.", 7);
+    }
+}
+
+// Ejemplo de arma: Bastón mágico
+public class MagicStaff extends Weapon {
+    public MagicStaff() {
+        super("Magic Staff", "A staff infused with magical power.", 12);
+    }
+}
