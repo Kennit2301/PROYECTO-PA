4203f3d44d103ec9eb534944c9c0d093f9dcc5bb
Kennit Saucedo <l22280944@toluca.tecnm.mx>
2024-10-12 22:04:41 -0600

    Create armaduras.java

---
+++ armaduras.java
@@ -0,0 +1,35 @@
+package items;
+
+// Ejemplo de armadura: Casco de hierro
+public class IronHelmet extends Armor {
+    public IronHelmet() {
+        super("Iron Helmet", "Provides basic head protection.", 5);
+    }
+}
+
+// Ejemplo de armadura: Pechera de acero
+public class SteelChestplate extends Armor {
+    public SteelChestplate() {
+        super("Steel Chestplate", "Heavy armor for torso protection.", 10);
+    }
+}
+
+// Ejemplo de armadura: Botas de cuero
+public class LeatherBoots extends Armor {
+    public LeatherBoots() {
+        super("Leather Boots", "Lightweight footwear for quick movement.", 3);
+    }
+}
+
+// Ejemplo de armadura: Guanteletes de plata
+public class SilverGauntlets extends Armor {
+    public SilverGauntlets() {
+        super("Silver Gauntlets", "Silver gloves that enhance grip and defense.", 4);
+    }
+
+// Ejemplo de armadura: Escudo de mithril
+public class MithrilShield extends Armor {
+    public MithrilShield() {
+        super("Mithril Shield", "A shield made of the rare mithril material.", 12);
+    }
+}
